6010858c233826ba465dfdb96bbf1c67a7dac140
abu-talib-786 <138965325+abu-talib-786@users.noreply.github.com>
2023-08-11 18:35:44 +0530

    initial commit

---
+++ src/components/TodoCreater.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(["create-todo", ]);
+
+const Todo = ref("");
+
+const createTodo = () => {
+    console.log('inside create todo');
+    emit("create-todo",Todo.value);
+};
+</script>
+
+<template>
+<div class="input-wrap">
+    <input type="text" v-model="Todo"/>
+    <button @click="createTodo()">Create</button>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
